06be1a9ae396e9eb1a909c8da416cbd3abe50fb3
ChongChong <83775648+DZ2584793260@users.noreply.github.com>
2022-06-17 20:27:26 +0800

    Update matrix.h

--- 期末Matrix/matrix.h
+++ 期末Matrix/matrix.h
@@ -5,7 +5,7 @@
 #include <complex>
 #include <cstring>
 #include <vector>
-//_Complex double  mycomplex2 = 2.000 + 3I;
+
 using namespace std;
 
 template<typename T>
@@ -18,14 +18,14 @@ private:
 
     T *data; //data是一个包含矩阵所有元素的一维动态数组。
 
-    int * ref_cout;
+    int * ref_count;
 
 public:
     int get_row();
 
     int get_col();
 
-    int get_ref_cout();
+    int get_ref_count();
 
     int *getShape();
 
@@ -43,13 +43,15 @@ public:
     matrix<T> operator*(matrix<T> &other);//矩阵叉乘
 
     matrix<T> operator*(T number);//scalar multiplication a*2
-    friend matrix<T> operator*(T number, matrix<T> &matrix1);//2*a
+
+    friend matrix<T> operator*(T number, matrix<T> & matrix1);//2*a
 
     matrix<T> operator/(T number);//scalar division a/2
+    
     matrix<T> transpose();//矩阵的转置
-    matrix<T> matrix_matrix_multiplication(matrix<T> &other);
+   
 
-    T dot(matrix<T> &other);//矩阵的点乘
+    T dot(matrix<T> &other);//矩阵的点乘，得到一个数字
     matrix<T> reshape(int newRow, int newCol);
 
     //截取矩阵第二三行，第二、三列  a[1:3,1:3]
@@ -64,6 +66,8 @@ public:
     vector<T> average(string axis = "no");
 
     matrix<T> conjugation(matrix<T>& matrix);
+
+    void display();
 };
 
 
